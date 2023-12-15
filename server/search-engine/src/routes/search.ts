7be39e10de9d2807243bca21de29abb6db3e1f1f
dwitidibyajyoti <dwitidibyajyoti@gmail.com>
2023-12-15 18:12:38 +0530

    aminitis search

--- server/search-engine/src/routes/search.ts
+++ server/search-engine/src/routes/search.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 // import { search } from "../../controllers/search.controllers";
-import { search } from "../controllers/search.controllers";
+import { search,searchAmenities } from "../controllers/search.controllers";
 
 
 const router: Router = Router();
 
 // Define a route for user registration
-router.get("/search", search);
+router.post("/search", search);
+router.post("/search-amenities", searchAmenities);
+
 
 // deleteEquipmentCategory
 export default router;
